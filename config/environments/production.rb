7c5865d5e526b0a39dd9ef8150a8f65a2b725514
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-09-15 17:37:26 -0500

    *****Typo in one line of production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.defaul_url_options = { host: 'https://malhotra.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://malhotra.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
