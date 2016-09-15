3e2f293e698d16326295b6ce20befe20117ef866
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-09-15 16:30:15 -0500

    FriendlyId setup for project postings URLs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    @project = Project.new(project_params)
+    @project = Project.new
   end
 
   def create
@@ -25,10 +25,10 @@ class ProjectsController < ApplicationController
 
 private
   def project_params
-    params.require(:project).permit(:title, :description, :link)
+    params.require(:project).permit(:title, :description, :link, :slug)
   end
 
   def find_project
-    @project = Project.find(params[:id])
+    @project = Project.friendly.find(params[:id])
   end
 end
