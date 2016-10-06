56569830bd036789be8803dbf8544bfd91594e4f
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-10-06 03:11:36 -0500

    Restiveness with Ruppal

--- app/assets/javascripts/welcome.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($) {
-  console.log('hello')
-
-  $('.wrapper').restive({
-    breakpoints: ['240', '320', '480', '640', '960'],
-    classes: ['css-240', 'css-320', 'css-480', 'css-640', 'css-960'],
-    force_dip: true
-  });
-})
